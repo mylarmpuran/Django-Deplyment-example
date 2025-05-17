3f5d29045b4baa1d46754c0b2c1e45d31fbc94e8
puran kumar <mylarmpuran@gmail.com>
2025-05-17 22:23:54 +0530

    "navbar"

---
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link"
+import Image from 'next/image'
+import SearchFilters from './SearchFilters'
+import UserNav from "./UserNav"
+
+
+const Navbar = () => {
+    return (
+        <nav className="w-full max-h-40 fixed top-0 left-0 py-6 border-b bg-white z-10">
+            <div className="max-w-[1500px] mx-auto px-6">
+                <div className="flex justify-between items-center">
+                    <Link href="/" >
+                    <Image
+                        src="/airbnblogo.jpeg"
+                        width={90}
+                        height={10}
+                        alt="DjangoBnb logo"
+                        />  
+                                 
+                    </Link>   
+
+
+                    <div className="flex space-x-6">
+                        <SearchFilters/>
+                    </div>
+
+                    <div className="flex items-center space-x-6">
+                        <UserNav/>
+                    </div>
+                         
+
+
+
+                </div>
+
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ djangobnb/app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,49 @@
+const SearchFilter = () => {
+    return (
+     <div className="h-[64px] flex flex-row items-center justify-between border-2 border-green-500 rounded-full">
+
+        <div>
+            <div className="flex flex-row items-center justify-between ">
+                <div className="cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center border-2 border-blue-500 rounded-full hover:bg-gray-100">
+                     <p className="text-xs font-semibold">Where</p>
+                    <p className="text-xs font-semibold">Wanted location</p>
+                </div>
+                
+                <div className=" cursor-pointer h-[64px] px-8 flex flex-col justify-center border-2 border-blue-500 rounded-full hover:bg-gray-100">
+                     <p className="text-xs font-semibold">Check in</p>
+                    <p className="text-xs font-semibold">Add dates</p>
+                </div>
+
+                <div className=" cursor-pointer h-[64px] px-8 flex flex-col justify-center border-2 border-blue-500 rounded-full hover:bg-gray-100">
+                     <p className="text-xs font-semibold">Check out</p>
+                    <p className="text-xs font-semibold">Add dates</p>
+                </div>
+
+                <div className=" cursor-pointer h-[64px] px-8 flex flex-col justify-center border-2 border-blue-500 rounded-full hover:bg-gray-100">
+                     <p className="text-xs font-semibold">Who</p>
+                    <p className="text-xs font-semibold">Add guests</p>
+                </div>
+                <div className="px-2">
+                <div className="cursor-pointer p-4 bg-airbnb hover:bg-airbnb-dark transition rounded-full text-white">
+                    <svg  viewBox="0 0 32 32" style={{
+                        display: 'block',
+                        fill: 'none',
+                        height: '16px',
+                        width: '16px',
+                        stroke: 'currentColor',
+                        strokeWidth: 4,
+                        overflow: 'visible',
+                        }} aria-hidden="true" role="presentation" focusable="false">
+                                            <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path>
+                    </svg>
+            </div>
+            </div>
+            
+        </div>
+     </div>
+     </div>
+    )
+}
+
+
+export default SearchFilter;
